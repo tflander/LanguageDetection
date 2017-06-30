a902968e3fb1f773a797da53acfeaad3ce003d0d
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2017-06-30 13:37:59 -0400

    clean up exception handling

--- src/main/java/todd/languageDetection/scan/LazyDictionaryScanner.java
+++ src/main/java/todd/languageDetection/scan/LazyDictionaryScanner.java
@@ -2,14 +2,9 @@ package todd.languageDetection.scan;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Set;
-
-import todd.languageDetection.reader.text.AsciiDictionaryReader;
 
 public class LazyDictionaryScanner implements DictionaryScanner {
 
@@ -43,12 +38,8 @@ public class LazyDictionaryScanner implements DictionaryScanner {
 					}
 				}
 		    }
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		} catch (Exception e) {
+			throw new IllegalStateException("failure reading "+ dictionaryFilename, e);
 		}
 		
 		return matches / strings.length;
